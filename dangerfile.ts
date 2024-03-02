c1b8a6fc6e78b1d1450505acb625f2fb6445c80e
Kodama Naoyuki <mmaga@dstribe.net>
2024-03-02 21:57:26 +0900

    Update dangerfile.ts

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,7 @@
 import { danger, fail } from "danger"
 
-const commits = danger.github.commits;
-commits.map(commit => {
+const commits = danger.git.commits;
+commits.forEach(commit => {
   if (commit.commit.message.startsWith('fixup')) {
     fail('Commit message contains "fixup".');
   }
